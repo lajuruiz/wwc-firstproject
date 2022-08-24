c74d8918bf9967fefbf04a76bbd227a222e315ad
Laura <laurajulianaru@gmail.com>
2022-08-24 15:59:40 -0500

    change dark and light

--- javascript/index.js
+++ javascript/index.js
@@ -1,26 +1,3 @@
-window.addEventListener('DOMContentLoaded', chistesAleatorios)
-
-const botton = document.getElementById("oneMore");
-
-if(botton) {
-    botton.addEventListener('click', chistesAleatorios)
-}
-
-function chistesAleatorios (){ 
-    const endpoint="https://api.dadjokes.io/api/random/joke";
-
-    fetch(endpoint)
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById("chiste").textContent= data.body[0].setup
-            document.getElementById("respuesta").textContent= data.body[0].punchline
-        }).catch(error => {
-            document.getElementById("chiste").textContent= "Uuuu, Parece que no hay chiste para ti"
-        })
-}
-
-
-
 // modo dark and light
 
 const buttonChange=document.getElementById("button-change");
